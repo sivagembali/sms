c2c7e2659626d9118f3d16c26bd2df74e717c6d8
Gembali Siva Prasada Rao <34371823+sivagembali@users.noreply.github.com>
2018-04-21 23:24:59 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -12,8 +12,8 @@ def messages():
     if(password == 'gvpmca'):
         mobiles = request.args.get('mobile')
         message = request.args.get('message')
-        authkey = "180622Acufr0Tf6Fg559ef6f69" # Your authentication key.
-        #mobiles = "9705707580,8978098160" # Multiple mobiles numbers separated by comma.
+        authkey = "180622Acufr0Tf6Fg559ef6f6969" # Your authentication key.
+        #mobiles = "970570758770,8978049668160" # Multiple mobiles numbers separated by comma.
         #message = "Status Demo" # Your message to send.
         sender = "MSGIND" # Sender ID,While using route4 sender id should be 6 characters long.
         route = "4" # Define route
